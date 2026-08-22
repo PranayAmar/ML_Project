4c97eaccdabc1129175b05b7520492deeb3e3a74
Meprasanna <prasannasingh27@gmail.com>
2026-08-21 20:08:48 -0700

    Initialize Express server and MongoDB connection
    Set up Express server with MongoDB connection and error handling.

--- Client/src/Pages/index.jsx
+++ Client/src/Pages/index.jsx
@@ -1,3 +1,55 @@
-export {default as Login} from './Login';
-export {default as Signup} from './Signup';
-export {default as Home} from './Home';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const authRoute = require('./Routes/AuthRoute.js');
+const { MONGO_URL, PORT } = process.env;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ['http://localhost:4000', 'http://localhost:5173'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+  })
+);
+
+app.use('/', authRoute);
+
+app.use((req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: 'Route not found.',
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  return res.status(500).json({
+    success: false,
+    message: 'Internal Server Error.',
+  });
+});
+console.log('Testing MongoDB connection...');
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    serverSelectionTimeoutMS: 5000,
+    connectTimeoutMS: 5000,
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully.');
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB Connection Error:');
+    console.error(err.message);
+  });
