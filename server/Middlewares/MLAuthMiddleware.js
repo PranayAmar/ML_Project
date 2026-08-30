7be194c7cac8160cef5b9c73805976a128b57b81
Meprasanna <prasannasingh27@gmail.com>
2026-08-30 02:43:53 -0700

    Update MLAuthMiddleware.js

--- server/Middlewares/MLAuthMiddleware.js
+++ server/Middlewares/MLAuthMiddleware.js
@@ -3,7 +3,10 @@ require("dotenv").config();
 module.exports.verifyMLService = (req, res, next) => {
   const serviceKey = req.headers["x-ml-service-key"];
 
-  if (!serviceKey || serviceKey !== process.env.ML_SERVICE_KEY) {
+  if (
+    !serviceKey ||
+    serviceKey !== process.env.ML_SERVICE_KEY
+  ) {
     return res.status(401).json({
       success: false,
       message: "Unauthorized ML service request.",
